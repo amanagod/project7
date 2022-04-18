9a958e9c09cca9d3721194afc5d96d9cc7b7d93c
Nishant Panchal <94225481+amanagod@users.noreply.github.com>
2022-04-18 17:16:34 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,9 +1,7 @@
-alert("hi");
-
+alert("hi sbsjhs");
 
 var a = document.querySelectorAll(".drum").length;
-var i;
-for(i=0; i<=a;i++){
+for( var i=0; i<a;i++){
  var b = document.querySelectorAll(".drum")[i];
 b.addEventListener("click",function (){
 var value=this.innerHTML;
@@ -22,37 +20,37 @@ animation(element);
 function sound(velue){
 switch(velue){
 case "w":
-var w= new Audio("crash.mp3");
+var w= new Audio('crash.mp3');
 w.play();
 break;
 
 case "a":
-var a= new Audio("kick-bass.mp3");
+var a= new Audio('kick-bass.mp3');
 a.play();
 break;
 
 case "s":
-var s= new Audio("snare.mp3");
+var s= new Audio('snare.mp3');
 s.play();
 break;
 
 case "d":
-var d= new Audio("tom-1.mp3");
+var d= new Audio('tom-1.mp3');
 d.play();
 break;
 
 case "j":
-var j= new Audio("tom-2.mp3");
+var j= new Audio('tom-2.mp3');
  j.play();
 break;
 
 case "k":
-var k = new Audio("tom-3.mp3");
+var k = new Audio('tom-3.mp3');
  k.play();
 break;
 
 case "l":
-var l= new Audio("tom-4.mp3");
+var l= new Audio('tom-4.mp3');
 l.play();
 break;
 }
