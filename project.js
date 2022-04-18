0d49b83e7a3d98101263198a60ffc5ed0a63d9d4
Nishant Panchal <94225481+amanagod@users.noreply.github.com>
2022-04-18 17:00:24 +0530

    Update project.js

--- project.js
+++ project.js
@@ -24,7 +24,7 @@ w.play();
 break;
 
 case "a":
-var a= new Audio("kick.mp3");
+var a= new Audio("kick-bass.mp3");
 a.play();
 break;
 
@@ -34,22 +34,22 @@ s.play();
 break;
 
 case "d":
-var d= new Audio("tom1.mp3");
+var d= new Audio("tom-1.mp3");
 d.play();
 break;
 
 case "j":
-var j= new Audio("tom2.mp3");
+var j= new Audio("tom-2.mp3");
  j.play();
 break;
 
 case "k":
-var k = new Audio("tom3.mp3");
+var k = new Audio("tom-3.mp3");
  k.play();
 break;
 
 case "l":
-var l= new Audio("tom4.mp3");
+var l= new Audio("tom-4.mp3");
 l.play();
 break;
 }
